a208570d477b7d35be60ee3317e7985f52268f4a
Jwsoat <jwsoat@users.noreply.github.com>
2019-08-28 22:26:06 +1200

    added page load time

--- footer.php
+++ footer.php
@@ -1,5 +1,12 @@
 <footer>
   <p> 2019 Jwsoat Website Design Services</p>
   <a href="https://twitter.com/jwsoat">Twitter</a>
+  <div class="datetime">
+    <p id="time">Page Loaded at:</p>
+    <script>
+    var d = new Date();
+    document.getElementById("time").innerHTML = d;
+    </script>
+  </div>
 </footer>
 </html>

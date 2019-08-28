f0fe84119d854bf04929a341c80000b0758f959c
Jwsoat <jwsoat@users.noreply.github.com>
2019-08-29 09:39:15 +1200

    addded rubbish

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ require 'head.php';
  ?>
 <h1> Welcome to the Jwsoat All in one Website</h1>
 <p> On this Website you will find lots of rubbish</p>
+<img src="img/rubbish.jpg">
 <?php
 require 'footer.php';
  ?>

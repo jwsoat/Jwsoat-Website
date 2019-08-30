6014df79ffc49b7b9ded2d582e473499f8649335
Jwsoat <jwsoat@users.noreply.github.com>
2019-08-30 15:52:43 +1200

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,7 +2,8 @@
   <p> 2019 Jwsoat Website Design Services</p>
   <a href="https://twitter.com/jwsoat">Twitter</a>
   <div class="datetime">
-    <p id="time">Page Loaded at:</p>
+    <p> Page Loaded on:</p>
+    <p id="time"></p>
     <script>
     var d = new Date();
     document.getElementById("time").innerHTML = d;
